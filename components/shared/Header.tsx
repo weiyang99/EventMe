1fad3b69533ec8e35e8594905c3314b1705aeb0f
weiyang <weiyangwygoh@gmail.com>
2024-03-08 15:07:14 +0800

    header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,10 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
     return (
@@ -15,8 +19,24 @@ const Header = () => {
                     />
                 </Link>
 
-                <div className='flex w-32 justify-end gap-3'>
+                <SignedIn>
+                    <nav className='md:flex-between hidden w-full max-w-xs'>
+                        <NavItems />
+                    </nav>
+                </SignedIn>
 
+                <div className='flex w-32 justify-end gap-3'>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl='/' />
+                        <MobileNav />
+                    </SignedIn>
+                    <SignedOut>
+                        <Button asChild className='rounded-full' size='lg'>
+                            <Link href="/sign-in">
+                                Login
+                            </Link>
+                        </Button>
+                    </SignedOut>
                 </div>
             </div>
         </header>
