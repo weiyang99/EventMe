97fea32de8bf52903f737738d6a2868c138d4b37
weiyang <weiyangwygoh@gmail.com>
2024-03-11 21:53:03 +0800

    profile

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,7 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
-// import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
 import { IOrder } from '@/lib/database/models/order.model'
 import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
@@ -15,9 +15,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
     const ordersPage = Number(searchParams?.ordersPage) || 1;
     const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-    // const orders = await getOrdersByUser({ userId, page: ordersPage })
+    const orders = await getOrdersByUser({ userId, page: ordersPage })
 
-    // const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
     const organisedEvents = await getEventsByUser({ userId, page: eventsPage })
 
     return (
@@ -35,20 +35,20 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
             <section className="wrapper my-8">
                 <Collection
-                    data={[]}
+                    data={orderedEvents}
                     emptyTitle="No event tickets purchased yet"
                     emptyStateSubtext="No worries - plenty of exciting events to explore!"
                     collectionType="My_Tickets"
                     limit={3}
                     page={ordersPage}
                     urlParamName="ordersPage"
-                    totalPages={1}
+                    totalPages={orders?.totalPages}
                 />
             </section>
 
             <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
                 <div className="wrapper flex items-center justify-center sm:justify-between">
-                    <h3 className='h3-bold text-center sm:text-left'>Events Organised</h3>
+                    <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
                     <Button asChild size="lg" className="button hidden sm:flex">
                         <Link href="/events/create">
                             Create New Event
